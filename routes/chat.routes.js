c60b70b398170b1a8feabfe9be60224607bf7ec0
Md Vadiyat Naqvi <127572794+mdvadiyatnaqvi@users.noreply.github.com>
2026-04-21 06:35:30 +0000

    storing the chat removed

--- routes/chat.routes.js
+++ routes/chat.routes.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {
-  sendMessage,
-  getHistory,
-  getConversation,
-} = require('../controllers/chat.controller');
+const { sendMessage } = require('../controllers/chat.controller');
 const {
   validateChatMessage,
-  validateUserId,
-  validateConversationId,
   handleValidationErrors,
 } = require('../middleware/validation');
 const { chatLimiter } = require('../middleware/rateLimiter');
 
 router.post('/', chatLimiter, validateChatMessage, handleValidationErrors, sendMessage);
-router.get('/history/:userId', validateUserId, handleValidationErrors, getHistory);
-router.get('/conversation/:id', validateConversationId, handleValidationErrors, getConversation);
 
 module.exports = router;
